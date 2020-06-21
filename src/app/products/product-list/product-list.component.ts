a67cd282b8e20b6887dee5094c20973025cf4eac
Nghiemducnam <nghiemducnam0312@gmail.com>
2020-06-21 15:31:49 +0700

    update product list component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AppState} from '../../reducers';
 import {select, Store} from '@ngrx/store';
-import {ProductListRequested} from '../../actions/product.action';
+import {ProductListReloaded, ProductListRequested} from '../../actions/product.action';
 import {isProductListLoaded, selectProductList} from '../../selectors/product.selector';
 import {Product} from '../../models/product';
 import {Observable} from 'rxjs';
-import {ProductService} from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -15,33 +14,20 @@ import {ProductService} from '../../services/product.service';
 export class ProductListComponent implements OnInit {
 products: Product[];
 isLoaded$: Observable<boolean>
-  constructor(private store: Store<AppState>,
-              private productService: ProductService) {
-  this.productService.productSubject$.subscribe(res =>{
-    if(res.data == '00'){
-      this.products = res.data;
-      console.log(this.products)
-      this.selectProducts();
-    }else
-      return;
-  })
+  constructor(private store: Store<AppState>) {
   }
 
   ngOnInit(): void {
     this.store.dispatch(new ProductListRequested());
-    // this.productService.callApiToGetProducts().subscribe(res => {
-    //   this.products = res.data;
-    //   console.log(this.products)
-    // })
-    this.selectProducts();
-  }
-
-  selectProducts(){
-    this.store.pipe(select(selectProductList)).subscribe((response: Product[]) => {this.products = response
-    ////////////
+    this.store.pipe(select(selectProductList)).subscribe(response => {this.products = response
+      ////////////
       console.log(this.products)
     })
     this.isLoaded$ = this.store.pipe(select(isProductListLoaded));
   }
 
+
+  update() {
+    this.store.dispatch(new ProductListReloaded())
+  }
 }
